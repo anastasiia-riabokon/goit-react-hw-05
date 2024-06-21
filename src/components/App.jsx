3ab89e155bab5d07e3e9c27b6979401bda5ed986
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-21 20:26:46 +0300

    feat: add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,36 @@
-import Navigation from "./Navigation/Navigation";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "../pages/HomePage";
+import MoviesPage from "../pages/MoviesPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import MovieDetailsPage from "../pages/MovieDetailsPage";
+import MovieCast from "./MovieCast";
+import MovieReviews from "./MovieReviews";
+import {useHTTP} from "./hooks/useHTTP";
 
 function App() {
+  const {data: movies} = useHTTP("trending/movie/day");
+
+  if (!movies) {
+    return <div>Loading...</div>;
+  }
+
+  const movieResults = movies ? movies.results : [];
+
   return (
     <>
-      <Navigation />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage movies={movieResults} />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="review" element={<MovieReviews />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }

af088059907779ee74427c011e3a0cc49d71e548
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-22 22:19:41 +0300

    feat: styles add page not found 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,19 @@
+import {Link} from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="text-center transform translate-y-full">
+      <div className="relative inline-block">
+        <h1 className="text-9xl font-bold text-gray-800 mb-4 relative z-10">
+          <span className="text-white">404</span>
+        </h1>
+        <div className="absolute inset-0 bg-gradient-to-br from-indigo-600 to-red-500 opacity-50 rounded-full blur-md animate-pulse"></div>
+      </div>
+      <p className="text-xl text-white mb-4 font-play">Page not found</p>
+      <Link to="/" className="bg-white p-2 rounded text-blue-950 font-play inline-block">
+        Go Home
+      </Link>
+    </div>
+  );
 };
 export default NotFoundPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        play: "Playwrite DE Grund, cursive",
+      },
+    },
   },
   plugins: [],
 };

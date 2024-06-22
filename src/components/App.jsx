560fc040eb497eb6315091553cac7b025eca8533
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-22 17:26:01 +0300

    feat: add async loading app routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import {Route, Routes} from "react-router-dom";
-import Layout from "./Layout";
-import HomePage from "../pages/HomePage";
-import MoviesPage from "../pages/MoviesPage";
-import NotFoundPage from "../pages/NotFoundPage";
-import MovieDetailsPage from "../pages/MovieDetailsPage";
-import MovieCast from "./MovieCast";
-import MovieReviews from "./MovieReviews";
+// import Layout from "./Layout";
+// import HomePage from "../pages/HomePage";
+// import MoviesPage from "../pages/MoviesPage";
+// import NotFoundPage from "../pages/NotFoundPage";
+// import MovieDetailsPage from "../pages/MovieDetailsPage";
+// import MovieCast from "./MovieCast";
+// import MovieReviews from "./MovieReviews";
 import {useHTTP} from "./hooks/useHTTP";
+import {Suspense, lazy} from "react";
+
+const Layout = lazy(() => import("./Layout.jsx"));
+const HomePage = lazy(() => import("../pages/HomePage"));
+const MoviesPage = lazy(() => import("../pages/MoviesPage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
+const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
+const MovieCast = lazy(() => import("./MovieCast"));
+const MovieReviews = lazy(() => import("./MovieReviews"));
 
 function App() {
   const {data: movies, isLoading, error} = useHTTP("trending/movie/day");
@@ -18,7 +27,7 @@ function App() {
   const movieResults = movies ? movies.results : [];
 
   return (
-    <>
+    <Suspense>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage movies={movieResults} />} />
@@ -31,7 +40,7 @@ function App() {
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </>
+    </Suspense>
   );
 }
 

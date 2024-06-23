2d3ac519f8a3f8f6a67f41abf669a450320779e5
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-23 12:49:56 +0300

    feat: add size for imgNot

--- src/helpers/ImgHTTP.jsx
+++ src/helpers/ImgHTTP.jsx
@@ -6,7 +6,7 @@ const ImgHTTP = ({src, w, h, alt}) => {
       {src ? (
         <img src={`https://image.tmdb.org/t/p/w500${src}`} alt={alt} width={w} height={h} />
       ) : (
-        <img src={imgNot} />
+        <img src={imgNot} alt="not image photo" width={w} height={h} />
       )}
     </>
   );
